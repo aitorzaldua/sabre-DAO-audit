3f2966fff73116aafbe67867781a4279ddc640e7
Aitor Zaldua <aitor.zaldua@draftdigital.xyz>
2024-04-05 18:24:13 +0200

    new sabreDAO

--- test/SabreDAOTest.t.sol
+++ test/SabreDAOTest.t.sol
@@ -4,6 +4,8 @@ pragma solidity 0.8.21;
 import {Test, console} from "./../lib/forge-std/src/Test.sol";
 
 import {SabreDAO} from "./../src/SabreDAO.sol";
+import {SabreDAOStaking} from "./../src/SabreDAOStaking.sol";
+import {SabreDAOEngine} from "./../src/SabreDAOEngine.sol";
 
 contract SabreDAOTest is Test {
     ////////////////////
@@ -21,36 +23,54 @@ contract SabreDAOTest is Test {
     // CONTRACTS   /////
     ////////////////////
     SabreDAO sabreDAO;
+    SabreDAOStaking staking;
+    SabreDAOEngine engine;
 
     ////////////////////
     // DEPLOY       /////
     ////////////////////
 
     function setUp() external {
-        vm.prank(deployer);
+        vm.startPrank(deployer);
         sabreDAO = new SabreDAO(deployer);
+
+        vm.stopPrank();
     }
 
     function test_checkAddresses() external view {
         console.log("sabreDao: ", address(sabreDAO));
         console.log("real Total supply:      ", sabreDAO.totalSupply());
-        console.log("variable s_TotalSupply: ", sabreDAO.s_TotalSupply());
         assertEq(deployer, sabreDAO.owner());
     }
 
     //////////////////////////////
     // TESTS  FUNCIONALES     ///
     /////////////////////////////
-    function test_checkMint() external {
+
+    /*1. MINT AND BURN */
+
+    function test_checkSimpleMint() external {
         vm.startPrank(deployer);
-        sabreDAO.mint(deployer, 50 ether);
-        console.log("real Total supply:      ", sabreDAO.totalSupply());
-        console.log("variable s_TotalSupply: ", sabreDAO.s_TotalSupply());
+        sabreDAO.mint(deployer, 50 * 1e18);
+        assertEq(110 * 1e18, sabreDAO.totalSupply());
+        assertEq(100 * 1e18, sabreDAO.balanceOf(deployer));
     }
 
-    /* function test_checkBurn() external {
+    function test_checkSimpleBurn() external {
         vm.startPrank(deployer);
-        sabreDAO.mint(deployer, 50 ether);
+        sabreDAO.burn(10 * 1e18);
+        assertEq(40 * 1e18, sabreDAO.balanceOf(deployer));
+    }
 
-        sabreDAO.burn(deployer, 20 ether); */
+    function test_burnMoreThanBalance() external {
+        vm.startPrank(deployer);
+        vm.expectRevert();
+        sabreDAO.burn(1000 * 1e18);
+    }
+
+    function test_mintToAddressZero() external {
+        vm.startPrank(deployer);
+        vm.expectRevert();
+        sabreDAO.mint(address(0), 50 * 1e18);
+    }
 }
